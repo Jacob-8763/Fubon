e49e35024428e6813e834e1e8c0a21474248fb13
Jacob-8763 <backup0464@gmail.com>
2024-11-18 18:16:12 +0800

    Add files via upload

---
+++ Shelter_vue/shelter/src/App.vue
@@ -0,0 +1,12 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+
+</script>
+
+<style scoped>
+</style>
